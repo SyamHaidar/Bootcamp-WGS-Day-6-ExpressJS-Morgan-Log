e8203e6624ac7d021eec31938dc51e94d38eb6f6
SyamHaidar <syamhaidar@gmail.com>
2022-09-09 10:35:49 +0700

    Create ExpressJS server with EJS view

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bootcamp WGS - Day 4
+
+### Lesson
+
+1. Create ExpressJS web server with EJS view
---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+// ---------------------------------------------------------------------------
+
+const app = express();
+const port = 3000;
+
+// EJS
+app.set("view engine", "ejs");
+
+// rooute
+app.get("/", (req, res) => {
+  res.render("index", {
+    name: "Syam",
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+// route with parameter
+app.get("/product/:id", (req, res) => {
+  res.send(
+    `<h3>Prodcut id: ${req.params.id} </br> Category: ${req.query.category}</h3>`
+  );
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.render("404");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
